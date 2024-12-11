9fb758a299829406cf33b21e9d8c863c1af469c6
Sreerag <sreekuttankottakkal@gmail.com>
2024-12-11 23:44:41 +0530

    new hosting

--- src/tmdb/tmdbConfig.js
+++ src/tmdb/tmdbConfig.js
@@ -1,22 +1,13 @@
-// export const API_KEY = import.meta.env.VITE_API_KEY
-// export const OMDB_KEY = import.meta.env.VITE_OMDB_KEY
+export const API_KEY = import.meta.env.VITE_API_KEY
+export const OMDB_KEY = import.meta.env.VITE_OMDB_KEY
 export const BASE_URL = "https://api.themoviedb.org/3/"
-export const API_KEY = typeof Deno !== "undefined"
-  ? Deno.env.get('VITE_API_KEY')
-  : import.meta.env.VITE_API_KEY;
 
-export const OMDB_KEY = typeof Deno !== "undefined"
-  ? Deno.env.get('VITE_OMDB_KEY')
-  : import.meta.env.VITE_OMDB_KEY;
-
-
-if (!API_KEY) {
+f (!API_KEY) {
   console.error("TMDB API_KEY is missing");
 }
 if (!OMDB_KEY) {
   console.error("OMDB API_KEY is missing");
 }
-console.log('Deno.env:', typeof Deno !== 'undefined' ? Deno.env.toObject() : 'Deno not available');
 
 
 
